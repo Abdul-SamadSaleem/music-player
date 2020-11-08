be1e33632ad83d6ea03e0a03aaaeaf04afe994a7
“Abdul <abdullahplayz07@gmail.com>
2020-11-08 14:43:40 +0000

    new changes

--- src/App.js
+++ src/App.js
@@ -1,15 +1,22 @@
+import React, {useState} from "react"
+
 // Import Styles
 import "./Styles/app.scss"
 // Import Components
 import Player from "./Components/Player"
 import Song from "./Components/Song"
+// Import util
+import data from "./util"
 
 
 const App = () => {
+  const [songs, setSongs] = useState(data())
+  const [currentSong, setCurrentSong] = useState(songs[0])
+  const [isPlaying, setIsPlaying] = useState(false)
   return (
     <div className="App">
-     <Song />
-     <Player />
+     <Song currentSong={currentSong} />
+     <Player setIsPlaying={setIsPlaying} isPlaying={isPlaying} currentSong={currentSong}/>
     </div>
   );
 }
