646460d602abbc10e9bfee77d771cde636c7462b
“Abdul <abdullahplayz07@gmail.com>
2020-11-08 12:55:17 +0000

    Made a boiler plate for the player component

---
+++ src/Components/Player.js
@@ -0,0 +1,13 @@
+export default function Player() {
+    return (
+        <div className="player">
+            <div className="time-control">
+                <p>Start Time</p>
+                <input type="range"/>
+                <p>End time</p>
+            </div>
+            <div className="play-cotrol">
+            </div>
+        </div>
+    )
+}
