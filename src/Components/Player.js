dd27b39edd9536caad0d669f675584e77a775c4e
“Abdul <abdullahplayz07@gmail.com>
2020-11-08 13:08:29 +0000

    Added all the font awesome Icons required

--- src/Components/Player.js
+++ src/Components/Player.js
@@ -1,3 +1,6 @@
+import { FontAwesomeIcon, fontAwesomeIcon } from '@fortawesome/react-fontawesome'
+import { faPlay, faAngleLeft, faAngleRight } from "@fortawesome/free-solid-svg-icons"
+
 export default function Player() {
     return (
         <div className="player">
@@ -7,6 +10,9 @@ export default function Player() {
                 <p>End time</p>
             </div>
             <div className="play-cotrol">
+                <FontAwesomeIcon className="skip-back" size="2x" icon={faAngleLeft} />
+                <FontAwesomeIcon className="play" size="2x" icon={faPlay} />
+                <FontAwesomeIcon className="skip-forward" size="2x" icon={faAngleRight} />
             </div>
         </div>
     )
