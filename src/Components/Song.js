1f26c6c3b0fe6eebd9ba5b3caefb7c9a0cc893ac
“Abdul <abdullahplayz07@gmail.com>
2020-11-08 12:52:54 +0000

    Made the boiler plate for the Song component

--- src/Components/Song.js
+++ src/Components/Song.js
@@ -1,8 +1,9 @@
-
 export default function Song() {
     return (
-        <div>
-            
+        <div className="song">
+            <h1>Picture</h1>
+            <h1>Song Name</h1>
+            <h1>Artist</h1>
         </div>
     )
 }
