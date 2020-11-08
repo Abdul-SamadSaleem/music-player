c97f79c0307bfa149169e6e482896369e697a168
“Abdul <abdullahplayz07@gmail.com>
2020-11-08 13:50:48 +0000

    spelling fix-up

--- src/Components/Player.js
+++ src/Components/Player.js
@@ -9,7 +9,7 @@ export default function Player() {
                 <input type="range"/>
                 <p>End time</p>
             </div>
-            <div className="play-cotrol">
+            <div className="play-control">
                 <FontAwesomeIcon className="skip-back" size="2x" icon={faAngleLeft} />
                 <FontAwesomeIcon className="play" size="2x" icon={faPlay} />
                 <FontAwesomeIcon className="skip-forward" size="2x" icon={faAngleRight} />
