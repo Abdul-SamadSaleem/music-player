bab753e86d45d6249165a713a219e584c65f401c
“Abdul <abdullahplayz07@gmail.com>
2020-11-08 22:12:30 +0000

    Made the library song component with some data

---
+++ src/Components/LibrarySong.js
@@ -0,0 +1,12 @@
+const LibrarySong = ({song}) => {
+    return (
+        <div className="library-song">
+            <img alt={song.name} src={song.cover} />
+            <div className="song-description">
+            <h3>{song.name}</h3>
+            <h3>{song.artist}</h3>
+            </div>
+        </div>
+    )
+}
+export default LibrarySong 
