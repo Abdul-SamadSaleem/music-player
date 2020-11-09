d3189ee73b559d7ea5f741b1084aca46a647b203
“Abdul <abdullahplayz07@gmail.com>
2020-11-09 21:35:59 +0000

    made navbar component

---
+++ src/Components/Nav.js
@@ -0,0 +1,13 @@
+// import React from 'react'
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome"
+import { faMusic } from "@fortawesome/free-solid-svg-icons"
+
+ const Nav = () => {
+    return (
+        <div>
+            
+        </div>
+    )
+}
+
+export default Nav
